9b87fa02400d59ceafa471fbab3786981d510e1e
Argha Chakrabarty <ac380012@gmail.com>
2021-12-12 05:13:03 +0530

    cors

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import pickle
 
@@ -10,7 +11,18 @@ def load_model():
     return pipeline
 
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class request_body(BaseModel):
     comment: str
